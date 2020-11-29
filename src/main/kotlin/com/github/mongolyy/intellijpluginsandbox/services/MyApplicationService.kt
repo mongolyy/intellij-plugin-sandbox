703ac9996cc1910b73878c25e6bea34a81376533
GitHub Action <action@github.com>
2020-11-29 07:50:12 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/mongolyy/intellijpluginsandbox/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mongolyy.intellijpluginsandbox.services
+
+import com.github.mongolyy.intellijpluginsandbox.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
